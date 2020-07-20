7fdafc97c095551999a68cb14a373bc8559e349e
Krishank-Tripathi <43090677+Krishank-Tripathi@users.noreply.github.com>
2020-07-20 13:45:14 +0530

    node Project for showing react-app on node server

---
+++ build/precache-manifest.6136741371fca3e49b2047e7ad45ac88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969eff38ace7b2f5261a973a035b34eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3adff02419cee401447",
+    "url": "/static/css/main.04ec11ee.chunk.css"
+  },
+  {
+    "revision": "11ad7ef6042259de147a",
+    "url": "/static/js/2.9e431e75.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.9e431e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3adff02419cee401447",
+    "url": "/static/js/main.3e2af267.chunk.js"
+  },
+  {
+    "revision": "5b4060d1d5e784799cd4",
+    "url": "/static/js/runtime-main.b85f6ef5.js"
+  },
+  {
+    "revision": "4b3d49b7ec21615af33421bfe2d6ff63",
+    "url": "/static/media/Glyphter.4b3d49b7.woff"
+  },
+  {
+    "revision": "7c28fe05152ece16e5da4da609098c73",
+    "url": "/static/media/Glyphter.7c28fe05.ttf"
+  },
+  {
+    "revision": "84552fba49c4717fcbdae4456b1ba10b",
+    "url": "/static/media/Glyphter.84552fba.eot"
+  },
+  {
+    "revision": "fe5bff552dd3369fbd37bdc11615fa99",
+    "url": "/static/media/Glyphter.fe5bff55.svg"
+  },
+  {
+    "revision": "d5be2f376ef67eb43cad2fc8fcae762c",
+    "url": "/static/media/loading.d5be2f37.svg"
+  },
+  {
+    "revision": "d5013f6cbc01832e2d2daacb3c962b10",
+    "url": "/static/media/no_avatar.d5013f6c.jpg"
+  },
+  {
+    "revision": "021b3a38af421576b6a7621e7f01e797",
+    "url": "/static/media/searching-magnifying-glass.021b3a38.svg"
+  }
+]);
